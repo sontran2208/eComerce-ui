2743aa926cc8429614a5b2ffa2a47b28cec0ddbe
sontran2208 <tranthanhson220804@gmail.com>
2025-01-27 01:10:53 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import CartPrimary from "./pages/CartPrimary";
 import DefaultLayout from "./components/layouts/DefaultLayout";
+import Product from "./pages/Product";
 function App() {
   return (
     <Router>
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/cart" element={<CartPrimary />} />
+            <Route path="/product" element={<Product />} />
           </Routes>
         </DefaultLayout>
       </div>
--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -1,5 +1,88 @@
-function Product({ name, cost, reviews, image, description, size }) {
-  return <div className="wrapper"></div>;
+import { Container, Row, Col } from "react-bootstrap";
+import Slider from "react-slick";
+import classNames from "classnames/bind";
+import styles from "./Product.module.scss";
+import { useState } from "react"; // Sửa lỗi import
+import imageService1 from "../../assets/img/others/services1.png";
+import imageService2 from "../../assets/img/others/services2.png";
+import imageService3 from "../../assets/img/others/services3.png";
+import imageService4 from "../../assets/img/others/services4.png";
+
+const cx = classNames.bind(styles);
+
+function Product() {
+  const images = [
+    { id: 1, src: imageService1, alt: "Service 1" },
+    { id: 2, src: imageService2, alt: "Service 2" },
+    { id: 3, src: imageService3, alt: "Service 3" },
+    { id: 4, src: imageService4, alt: "Service 4" },
+  ];
+
+  const [selectedImage, setSelectedImage] = useState(images[0].src); // Sửa state để nhận src
+
+  const sliderSettings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    arrows: true,
+  };
+
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("content")}>
+        <Container>
+          <Row>
+            <Col md={6}>
+              <div className={cx("left")}>
+                <Container>
+                  <Row>
+                    <Col md={8}>
+                      <div className={cx("image")}>
+                        <img src={selectedImage} alt="Selected" />
+                      </div>
+                    </Col>
+                    <Col md={4}>
+                      <div className={cx("slider")}>
+                        <Slider {...sliderSettings}>
+                          {images.map((image, index) => (
+                            <div
+                              key={index}
+                              onClick={() => setSelectedImage(image.src)} // Chỉ lấy src của ảnh
+                              style={{
+                                cursor: "pointer",
+                                padding: "5px",
+                                border:
+                                  selectedImage === image.src
+                                    ? "2px solid var(--primary)"
+                                    : "none",
+                              }}
+                            >
+                              <img
+                                src={image.src} // Hiển thị đúng src của ảnh
+                                alt={image.alt}
+                                style={{ width: "100%" }}
+                              />
+                            </div>
+                          ))}
+                        </Slider>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className={cx("right")}></div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
